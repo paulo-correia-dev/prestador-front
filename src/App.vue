5b39c8fb6f0a23718ae8f306228acb3a63263717
Paulo Correia <programmerpaulo@gmail.com>
2023-03-06 15:11:23 -0300

    upload da aplicacao front

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template>  
+
+  <nav>    
+    <router-link to="/">
+      <q-btn label="Página Inicial" color="blue" icon="home" flat="false"/>
+    </router-link>
+
+    <router-link to="/consulta">
+      <q-btn label="Consulta" color="blue" icon="search" />
+    </router-link>
+
+    <router-link to="/cadastro">
+      <q-btn label="Cadastro" color="blue" icon="person_add" />
+    </router-link>
+  </nav>
+  <router-view/>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+
+  data(){
+
+    return{
+
+     
+
+    }
+
+  },
+
+  mounted(){
+    
   }
+  
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style lang="scss">
+#app {  
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
+}
+
+nav {
+  padding: 30px;
+ 
 }
 </style>
